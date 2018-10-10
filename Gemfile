3fc0ef14f399d8adcaed9a187771e74966b94479
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-10 09:41:09 -0700

    upgrade to ruby 2.5.1

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
-ruby "2.4.0"
+ruby "2.5.1"
 gem 'github-pages'
--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ echo "Installing software needed to run Jekyll locally... "
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
-rvm install ruby-2.4.0
-rvm use ruby-2.4.0
+rvm install ruby-2.5.1
+rvm use ruby-2.5.1
 gem install bundler
 bundle install --path vendor/bundle
 echo "Done."
